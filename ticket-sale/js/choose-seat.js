ea34955a663077a2a398e435b4517c3e2149e49a
hylerrix <hylerrix@foxmail.com>
2018-06-08 01:47:20 +0800

    feat(seat): we can select seat now

--- ticket-sale/js/choose-seat.js
+++ ticket-sale/js/choose-seat.js
@@ -1,3 +1,139 @@
+window.onload = function () {
+    // 初始化已选座位
+    layui.data('ticket-sale', {
+        key: 'positionSelected'
+        ,value: []
+    });
+
+    let seatInfo = loadSeatInfo()
+    renderSeatInfo(seatInfo)
+}
+
+// 座位信息绑定在全局 window 对象上
+
+
+// 0: 没座位，1: 可选座位，2: 已选座位，3: 已售座位
+function loadSeatInfo () {
+     return [
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    ]
+}
+
+function renderSeatInfo (seatInfo) {
+    let domString = ''
+    seatInfo.forEach(function (seatRow, i) {
+        domString += `<dl class="row${ i + 1 }">`
+        seatRow.forEach(function (seatCol, j) {
+            let statusClass = getStatusClass(seatInfo[i][j])
+            domString += `<dd><div class="${ statusClass }" data-position="${i + '-' + j}" data-status="${ seatInfo[i][j] }" onclick="clickOneSeat(this)"></div></dd>`
+        })
+        domString += `</dl>`
+    })
+    $('#sittingList').append($.parseHTML(domString))
+}
+
+function clickOneSeat (el) {
+    let position = el.dataset.position.split('-')
+    // 0: 没座位，1: 可选座位，2: 已选座位，3: 已售座位
+    let status = parseInt(el.dataset.status)
+    let ticketSale = layui.data('ticket-sale')
+    let positionSelected = ticketSale.positionSelected
+
+    if (status === 1) {
+        // 本身可选，变成已选
+
+        // 判断是否超过 5 个
+        if (positionSelected.length > 4) {
+            layui.use(['layer'], function () {
+                layer.msg('最多选择五个座位！')
+            })
+            return
+        }
+
+        el.dataset.status = 2
+        el.className = getStatusClass(2)
+        // 更新右侧已选座位信息
+        positionSelected.push(position)
+        layui.data('ticket-sale', {
+            key: 'positionSelected'
+            ,value: positionSelected
+        });
+        renderPositionInfo()
+    } else if (status === 2) {
+        // 本身已选，变成可选，更新右侧已选座位信息
+        el.dataset.status = 1
+        el.className = getStatusClass(1)
+        // 更新右侧已选座位信息
+        let afterFilter = positionSelected.filter(v => {
+            // 删除该已选座位信息
+            return !arraysEqual(v, position)
+        })
+        layui.data('ticket-sale', {
+            key: 'positionSelected'
+            ,value: afterFilter
+        });
+        renderPositionInfo()
+    }
+}
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+
+function getStatusClass (number) {
+    let statusClass = ''
+    switch (parseInt(number)) {
+        case 0:
+            statusClass = 'nosit'
+            break;
+        case 1:
+            statusClass = 'kx'
+            break;
+        case 2:
+            statusClass = 'yx'
+            break;
+        case 3:
+            statusClass = 'ys'
+            break;
+    }
+    return statusClass
+}
+
+function renderPositionInfo () {
+    let ticketSale = layui.data('ticket-sale')
+    let positionSelected = ticketSale.positionSelected
+
+    // clear
+    $('#selectedSeat').empty()
+
+    // render
+    let domString = ''
+    positionSelected.forEach(v => {
+        domString += `<ol>${v[0]} 排 ${v[1]} 座</ol>`
+    })
+    $('#selectedSeat').append($.parseHTML(domString))
+}
+
 function Step03_selectSeat (el) {
     console.log(el)
 
